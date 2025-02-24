cc727034df9e8c5def3340d47a358f06caade1af
Anyr00d <anirudh.santhanam2022@vitstudent.ac.in>
2025-02-24 18:26:19 +0530

    complete frontend components

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Appbar } from '../Components/Appbar'
+import { Balance } from '../Components/Balance'
+import { Users } from '../Components/Users'
+
+export const Dashboard = () => {
+  return (
+    <div>
+      <Appbar/>
+      <div className='m-8'>
+          <Balance value={"10,000"}/>
+          <Users/>
+      </div>
+    </div>
+  )
+}
